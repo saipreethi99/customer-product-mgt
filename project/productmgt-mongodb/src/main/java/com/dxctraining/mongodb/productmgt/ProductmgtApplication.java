49c6af43ed605560f5d78a1834613907317cc5de
csaipreethi <saipreethi1212@gmail.com>
2020-09-02 23:47:12 +0530

    sprint 2

---
+++ project/productmgt-mongodb/src/main/java/com/dxctraining/mongodb/productmgt/ProductmgtApplication.java
@@ -0,0 +1,16 @@
+package com.dxctraining.mongodb.productmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories
+public class ProductmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmgtApplication.class, args);
+
+	}
+
+}
